3d76afb0522d747a7bf08490545ca31826cb12fc
Yifan Wang <wangyifan1995@gmail.com>
2019-10-08 13:51:56 -0700

    sync

---
+++ gpu_monitor_flask.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+import time
+app = Flask(__name__)
+
+@app.route('/')
+def gpu_monitor_server():
+    with open('info.pkl', 'rb') as f:
+        info = pickle.load(f)
+
+    results = info['serverInfo']
+    timestamp = info['timestamp']
+    return render_template('files.html', serverInfo=results, timestamp=timestamp)
---
+++ saveGPUinfo.py
@@ -0,0 +1,34 @@
+import time
+import pickle
+from datetime import datetime
+from gpu_monitor import getServerInfo
+
+def gpu_monitor_server(serverList):
+    serverInfo = getServerInfo(serverList)
+
+    results = []
+    serverNames = serverList[1:]
+    for server in serverNames:
+        tmp = [server]
+        try:
+            tmp.append(serverInfo[server])
+        except:
+            tmp.append(["This server is down! Keep calm and email Stephen."])
+        results.append(tmp)
+
+    timestamp = time.time()
+    timestamp = datetime.fromtimestamp(timestamp)
+
+    return {'serverInfo': results, 'timestamp': timestamp}
+
+
+def saveInfo():
+    serverList = ['-l', 'nescafe.cs.washington.edu', 'sanka.cs.washington.edu',
+                        'arabica.cs.washington.edu', 'chemex.cs.washington.edu',
+                        'lungo.cs.washington.edu', 'ristretto.cs.washington.edu']
+
+    while True:
+        info = gpu_monitor_server(serverList)
+        with open('info.pkl', 'wb') as f:
+            pickle.dump(info, f)
+        time.sleep(20)
