22d6b7bdf31bada180a79dc049b7299033ce26e9
Yifan Wang <yifan1@cs.washington.edu>
2019-10-09 16:23:40 -0700

    sync

--- runMonitor.sh
+++ runMonitor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export FLASK_APP=$(pwd)/gpu_monitor_flask.py
-flask run --host=0.0.0.0 --port=9527
-python3 saveGPUinfo.py
+flask run --host=0.0.0.0 --port=9527 &
+python3 saveGPUinfo.py &
