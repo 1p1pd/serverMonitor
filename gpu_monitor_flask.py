a35d608434f3a7d5ab7b8a86a25fad3f2c480fcc
Yifan Wang <wangyifan1995@gmail.com>
2019-10-23 20:57:59 -0700

    sync

--- gpu_monitor_flask.py
+++ gpu_monitor_flask.py
@@ -9,5 +9,16 @@ def gpu_monitor_server():
         info = pickle.load(f)
 
     results = info['serverInfo']
+    user_count = {}
+    for server, status in results:
+        for s in status:
+            if 'used by' in s:
+                username = s.split('used by')[1].split('(')[0].strip()
+                try:
+                    user_count[username] += 1
+                except Exception as e:
+                    user_count[username] = 1
+    user_count = [(k, v) for k, v in user_count.items()]
+    user_count.sort(key=lambda i: i[1], reverse=True)
     timestamp = info['timestamp']
-    return render_template('files.html', serverInfo=results, timestamp=timestamp)
+    return render_template('files.html', userCount=user_count, serverInfo=results, timestamp=timestamp)
