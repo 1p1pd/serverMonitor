00982175fe2f4943b957152508de4a2c3ca55643
Yifan Wang <wangyifan1995@gmail.com>
2020-04-25 19:12:32 -0700

    add color

--- gpu_monitor_flask.py
+++ gpu_monitor_flask.py
@@ -1,7 +1,28 @@
 import pickle
-from flask import Flask, render_template
+from flask import Flask, Markup, render_template
 
 app = Flask(__name__)
+
+def add_color(info):
+    # user_count[user] = [0,
+    #                     '{:.2f}% utilization'.format(util),
+    #                     []]
+    k, n, u, v = info
+    if n > 8:
+        n = Markup('<red>{} GPUs</red>'.format(n))
+    elif n <= 8 and n > 4:
+        n = Markup('<yellow>{} GPUs</yellow>'.format(n))
+    elif n <= 4 and n > 1:
+        n = Markup('{} GPUs'.format(n))
+    else:
+        n = Markup('{} GPU'.format(n))
+    if u > 80:
+        u = Markup('{:.2f}% utilization'.format(u))
+    elif u <= 80 and u > 40:
+        u = Markup('<yellow>{:.2f}%</yellow> utilization'.format(u))
+    else:
+        u = Markup('<red>{:.2f}%</red> utilization'.format(u))
+    return (k, n, u, v)
 
 @app.route('/')
 def gpu_monitor_server():
@@ -41,8 +62,7 @@ def gpu_monitor_server():
     user_server = [(k1, k2, v) for (k1, k2), v in user_server.items()]
     user_count = {}
     for user in users:
-        util = user_util[user]
-        user_count[user] = [0, '{:.2f} % utilization'.format(sum(util) / len(util)), []]
+        user_count[user] = [0, sum(user_util[user]) / len(user_util[user]), []]
 
     for user in users:
         for username, servername, n_gpu in user_server:
@@ -50,15 +70,9 @@ def gpu_monitor_server():
                 user_count[username][0] += n_gpu
                 user_count[username][2] += ['{} * {}'.format(servername, n_gpu)]
 
-    user_count = [(k, n, u, ', '.join(v)) for k, [n, u, v] in user_count.items()]
-
-    user_red = [i for i in user_count if (i[1] > 8)]
-    user_yellow = [i for i in user_count if (i[1] <= 8 and i[1] > 4)]
-    user_rest = [i for i in user_count if (i[1] <= 4)]
-    user_red.sort(key=lambda i: i[1], reverse=True)
-    user_yellow.sort(key=lambda i: i[1], reverse=True)
-    user_rest.sort(key=lambda i: i[1], reverse=True)
-    user_count = [user_red, user_yellow, user_rest]
+    user_count = [[k, n, u, ', '.join(v)] for k, [n, u, v] in user_count.items()]
+    user_count.sort(key=lambda i: i[1], reverse=True)
+    user_count = [add_color(i) for i in user_count]
 
     timestamp = info['timestamp']
     return render_template('files.html', userCount=user_count,
